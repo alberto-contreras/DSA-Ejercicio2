71048289f6b24f69eaad738689c18cc0749904fd
alberto-contreras <contreras.m.alberto@gmail.com>
2019-10-11 12:34:02 +0200

    Functions and test OK

--- src/main/java/QueueImpl.java
+++ src/main/java/QueueImpl.java
@@ -2,42 +2,53 @@ import java.util.LinkedList;
 
 public class QueueImpl<E> implements Queue<E> {
 
-    E[] data;//creamos un vector el cual forma la cola
+    /**
+     * creamos un vector el cual forma la cola
+     */
+    E[] data;
     int p;//indice que nos dice cuantos elementos hay actualmente en la cola
 
     public QueueImpl(int len) {//constructor
 
         this.data = (E[])new Object[len];//se crea la cola con el tamaño que nos pasen en el constructor
         this.p = 0;//el numero de elementos es cero
+
     }
 
+    /**
+     * Mètode que ....
+     * @param e element a afegir
+     * @throws QueueFullException llença una excepció en el cas que la cua estigui plena
+     */
     public void push(E e) throws QueueFullException{
 
-        if (this.isFull()) throw new QueueFullException();//si la cola esta llena enviar error
-
-        data[p] = e;//añadimos el elemento e incrementamos el indice
-        p = p++;
-
-    }
-
-    private boolean isFull() {//comprobar si la cola esta llena
-        //p  / this.data.length
-        if ((p/this.data.length)<1) {
-            return false;
+        if (this.isFull()) {
+  //          log.warn("Cua Plena")
+            throw new QueueFullException();//si la cola esta llena enviar error
         }
-        else
-            return true;
+
+//        log.info("afegim "+e +" a pos"+p);
+        data[p] = e;//añadimos el elemento e incrementamos el indice
+        p++;
+
     }
 
+    /**
+     *
+     * @return
+     * @throws QueueEmptyException
+     */
     public E pop() throws QueueEmptyException {//Eliminamos el primer elemento de la cola y desplazamos todos los elementos una posición
         if (this.isEmpty()) throw new QueueEmptyException();
 
         E res = this.data[0];
+//        log.info("desencuem "+res); //
         p=p-1;
         lshift();
 
         return res;
     }
+
 
     private void lshift() {//desplazamos todos los elementos en una posicion
         int i=0;
@@ -45,15 +56,13 @@ public class QueueImpl<E> implements Queue<E> {
             data[i]=data[i+1];
             i++;
         }
-
     }
 
+    private boolean isFull() {//comprobar si la cola esta llena
+        return (p>=this.data.length);
+    }
     private boolean isEmpty() {
-        if(this.size()==0) {
-            return true;
-        }
-        else
-            return false;
+        return (this.size()==0);
     }
 
     public int size() {
